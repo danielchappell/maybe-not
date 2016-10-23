383902012ebba785ad1638720389b09b758327d2
Daniel Chappell <daniel.chappell@q2ebanking.com>
2016-10-23 12:52:50 -0500

    change typescript config to use es6 modules

--- bundle.js
+++ bundle.js
@@ -46,11 +46,15 @@
 
 	"use strict";
 
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-	var Maybe = function () {
+	var Maybe = exports.Maybe = function () {
 	    function Maybe(type, value) {
 	        _classCallCheck(this, Maybe);
 
@@ -62,8 +66,8 @@
 	    _createClass(Maybe, [{
 	        key: "fmap",
 	        value: function fmap(fn) {
-	            if (this.isSomething()) {
-	                return Maybe.maybe(fn(this.value));
+
+                if (this.isSomething()) { return Maybe.maybe(fn(this.value));
 	            }
 	            return Maybe.nothing();
 	        }
@@ -161,8 +165,6 @@
 
 	    return Maybe;
 	}();
-
-	exports.Maybe = Maybe;
 
 /***/ }
 /******/ ]);
